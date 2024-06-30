295cd918c8f1760733f00b93eaf0c4b88446cc53
Dhruv Pandey <dhruvpandey2320@gmail.com>
2024-07-01 00:51:35 +0530

    resolve bugs in user router

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -36,8 +36,8 @@ router.route("/refresh-token").post(refreshAccessToken);
 router.route("/change-password").post(verifyJWT,changeCurrentPassword);
 router.route("/current-user").get(verifyJWT,getCurrentUser);
 router.route("/update-account").patch(verifyJWT, updateAccountDetails);
-router.route("update-avatar").patch(verifyJWT,upload.single("avatar"), updateUserAvatar);
-router.route("update-coverImage").patch(verifyJWT,upload.single("coverImage"), updateUserCoverImage);
+router.route("avatar").patch(verifyJWT,upload.single("avatar"), updateUserAvatar);
+router.route("cover-image").patch(verifyJWT,upload.single("coverImage"), updateUserCoverImage);
 router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
 router.route("/history").get(verifyJWT, getWatchHistory);
 
