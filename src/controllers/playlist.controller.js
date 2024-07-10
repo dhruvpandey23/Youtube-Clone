8a448efb8a9839820c477a463c5228374977e142
Dhruv Pandey <dhruvpandey2320@gmail.com>
2024-07-10 23:11:33 +0530

    add some features to playlist Controler

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -26,16 +26,38 @@ const createPlaylist = asyncHandler(async (req, res) => {
 
 const getUserPlaylists = asyncHandler(async (req, res) => {
     const {userId} = req.params
+    if(!userId) throw new ApiError(400,"Enter valid userId");
+    const userPlaylists = await Playlist.find({owner:userId});
+    if(!userPlaylists) throw new ApiError(400,"Not Found any Playlist");
+    return res.status(201).json(new ApiResponse(201,userPlaylists,"Users all playlist is fetched"))
     //TODO: get user playlists
 })
 
 const getPlaylistById = asyncHandler(async (req, res) => {
     const {playlistId} = req.params
+    if(!playlistId) throw new ApiError(400,"Enter the correct playlistId");
+    const playlist = await Playlist.findById(playlistId);
+    if(!playlist) throw new ApiError(400,"Playlist is not found");
+    return res.status(201).json(new ApiResponse(201,playlist,"Playlist fetched successfully"))
     //TODO: get playlist by id
 })
 
 const addVideoToPlaylist = asyncHandler(async (req, res) => {
     const {playlistId, videoId} = req.params
+    if([playlistId,videoId].some((field)=>field?.trim()==="")) throw new ApiError(400,"Enter VideoId and PlaylistId");
+    const video = await Video.findById(videoId);
+    if(!video) throw new ApiError(400,"Video is not found");
+    const playlist = await Playlist.findByIdAndUpdate(playlistId,
+        {
+          $push:{
+            videos:video,
+          }
+        },{new:true}
+    );
+    // playlist.videos.push(video);
+
+    return res.status(201).json(new ApiResponse(201,playlist,"Video is added in the playlist"))
+
 })
 
 const removeVideoFromPlaylist = asyncHandler(async (req, res) => {
